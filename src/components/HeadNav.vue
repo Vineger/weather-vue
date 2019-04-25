7fafc957f952b9b3e1a329d25328e2b0ac2570f8
Vineger <gdchhkf@163.com>
2019-04-25 11:44:08 +0800

    修改头部导航栏

--- src/components/HeadNav.vue
+++ src/components/HeadNav.vue
@@ -1,10 +1,22 @@
 <template>
-  <el-menu mode="horizontal" :default-active="'3'" @select="handleSelect">
-    <el-menu-item v-if="!this.state" index="1">注册</el-menu-item>
-    <el-menu-item v-if="!this.state" index="2">登陆</el-menu-item>
-    <el-menu-item v-if="this.state" index="3">{{this.name}}</el-menu-item>
+  <el-menu 
+    mode="horizontal"
+    :default-active="'1'"
+    text-color="#000000"
+    @select="handleSelect">
+    <el-menu-item index="1">天气分析系统</el-menu-item>
+    <el-menu-item v-if="!this.state" index="2" style="float:right; margin-right:30px">注册</el-menu-item>
+    <el-menu-item v-if="!this.state" index="3" style="float:right">登陆</el-menu-item>
+    <el-menu-item v-if="this.state" index="4" style="float:right">{{this.name}}</el-menu-item>
   </el-menu>
 </template>
+
+<style scoped>
+.el-menu {
+  font-weight: 600;
+}
+</style>
+
 
 <script>
 export default {
